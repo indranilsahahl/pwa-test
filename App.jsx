2016f9efc70a9c64ee0a4fe7e9091281fa118a8b
Indranil Saha <indranil.saha@gmail.com>
2025-08-13 21:51:33 +0530

    Initial Commit - PWA code base ChatGPT5 generated

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>🚀 My First PWA</h1>
+      <p>This app works offline!</p>
+    </div>
+  )
+}
