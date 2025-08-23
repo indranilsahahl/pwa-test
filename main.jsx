e93199c10493af226b68ceca6ce103a45715d48d
Indranil Saha <indranil.saha@gmail.com>
2025-08-23 15:14:14 +0530

    Dev checkpoint global_t1.css and login.css

--- main.jsx
+++ main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'global_t1.css'
+import 'login.css'
 import { registerSW } from 'virtual:pwa-register'
 
 registerSW({ immediate: true })
