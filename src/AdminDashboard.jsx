f802fff7d71e4376e0ad1b47c2f4a905fc62347c
Indranil Saha <indranil.saha@gmail.com>
2025-08-30 03:31:24 +0530

    Back to CSS... Swal dailog in Admin approval corrected call

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -180,10 +180,10 @@ const showConfirmDialog = async ({ title, text, confirmText, icon = "question", 
            	{/* Approve / Reject row */}
       	<tr>
         <td colSpan={3} className="text-center">
-          <button className="gb_btn_mediumseagreen" onClick={() =>
-                          handleApprove(row.empid, row.which_date)}> Approve </button>
-          <button className="gb_btn_red" onClick={() =>
-                          handleReject(row.empid, row.which_date)}>  Reject </button>
+          <button className="gb_btn_mediumseagreen" 
+          	onClick={() => confirmAction(empid, which_date, "approve")}> Approve </button>
+          <button className="gb_btn_red" 
+          	onClick={() => confirmAction(empid, which_date, "reject")}>  Reject </button>
         </td>
         </tr>
               </React.Fragment>
