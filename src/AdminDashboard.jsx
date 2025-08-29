14b6ea8614ff52000f5fff0119df2f362291a65a
Indranil Saha <indranil.saha@gmail.com>
2025-08-30 04:05:44 +0530

    Handle Back Button

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -26,6 +26,23 @@ export default function AdminDashboard({reloadTrigger}) {
 
   // Fetch Pending Logs 
   useEffect(() => {
+    
+    // Listen for back button / browser navigation
+    const handleBackButton = (event) => {
+      event.preventDefault();
+      // Trigger logout
+      sessionStorage.clear();
+      // Optionally navigate to login page
+      navigate("/", { replace: true });
+    };
+
+    window.addEventListener("popstate", handleBackButton);
+
+    return () => {
+      window.removeEventListener("popstate", handleBackButton);
+    };
+  }, [navigate]);
+    
     if (!otpVerified) return; // skip until verified
     
     const pendingLogs = async () => {
@@ -112,23 +129,6 @@ const showConfirmDialog = async ({ title, text, confirmText, icon = "question", 
   if (res.isConfirmed && typeof onConfirm === "function") {
     await onConfirm();
   }
-  
-  // Listen for back button / browser navigation
-    const handleBackButton = (event) => {
-      event.preventDefault();
-      // Trigger logout
-      sessionStorage.clear();
-      // Optionally navigate to login page
-      navigate("/", { replace: true });
-    };
-
-    window.addEventListener("popstate", handleBackButton);
-
-    return () => {
-      window.removeEventListener("popstate", handleBackButton);
-    };
-  }, [navigate]);
-  
 };
  
   // --- render ---
