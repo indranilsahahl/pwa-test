2521acc06da269635ad4dfc803912ff67a9d50ee
Indranil Saha <indranil.saha@gmail.com>
2025-08-30 00:42:25 +0530

    Tailwind Migration With bg-cover fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  extend: {
+  theme: {
+    extend: {
       backgroundImage: {
         'app-gradient': "linear-gradient(to top, rgba(69,192,174,1), rgba(69,192,174,0.2))",
       },
+    },
+  },
   plugins: [],
 };
+
