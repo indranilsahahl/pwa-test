300a83b1ad878b3aa8ed0f2059a9ea6dd6db4b2d
Indranil Saha <indranil.saha@gmail.com>
2025-08-30 23:34:16 +0530

    API Key issue fix -done. Now version button

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       workbox: { cleanupOutdatedCaches: true },
       manifest: {
         name: 'EsAttendance',
